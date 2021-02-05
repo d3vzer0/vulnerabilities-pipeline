3c5927f284762541aaee592f250c602293751d60
Joey Dreijer <jdreijer@schubergphilis.com>
2021-02-05 22:32:21 +0100

    Added support for ES tls

--- solids/elastic/resource.py
+++ solids/elastic/resource.py
@@ -1,19 +1,23 @@
 from dagster import String, StringSource, resource
 from elasticsearch import Elasticsearch, helpers
+from ssl import create_default_context
 
 @resource(
     config_schema={
         'uri': StringSource,
+        'ca_path': StringSource,
     }
 )
 def es_resource(init_context):
     class ESResource:
         def __init__(self, resource_config):
             self.uri = resource_config['uri']
+            self.ca_path = resource_config['ca_path']
  
         @property
         def client(self):
-            handler = Elasticsearch([self.uri])
+            context = create_default_context(cafile=self.ca_path)
+            handler = Elasticsearch([self.uri], ssl_context=context)
             return handler
 
     return ESResource(init_context.resource_config)
